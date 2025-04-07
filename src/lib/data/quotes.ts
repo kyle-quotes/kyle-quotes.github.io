590d47bea5d6a9906b2694256451aa83d93c4392
Nico Lehmann <nico.lehmannm@gmail.com>
2025-04-06 20:20:47 -0700

    Add more quotes

--- src/lib/data/quotes.ts
+++ src/lib/data/quotes.ts
@@ -6,6 +6,13 @@ export interface Quote {
 
 export const quotes: Quote[] = [
 	{
+		text: 'Imagine Harry Potter but all the wands are mouses'
+	},
+	{
+		text: 'Mnemonic is not a word that should start with the letter M. Words that should start with Ms are like "mom". Or "map"',
+		context: 'Master class in the International Symposium of Advanced Linguistics'
+	},
+	{
 		text: 'I’m a moist guy',
 		context: 'referring to his feet after climbing',
 		date: '2025-04-06'
