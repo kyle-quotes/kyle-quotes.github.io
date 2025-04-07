43bf79cccf4a95c7e05c67654ee2fbcc19c2e891
Nico Lehmann <nico.lehmannm@gmail.com>
2025-04-06 19:51:44 -0700

    setup adapter

---
+++ kyle-quotes-app/src/lib/data/quotes.ts
@@ -0,0 +1,15 @@
+export interface Quote {
+	text: string;
+	context?: string;
+	date?: string;
+}
+
+export const quotes: Quote[] = [
+  {
+    text: ""
+  },
+	{
+		text: 'I’m a moist guy',
+		date: '2025-04-06'
+	}
+];
